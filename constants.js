c5fbcb2508614843cc6b585d264aa7a58c8c05a7
James Pickard <james.pickard@lightspeedpos.com>
2016-05-18 17:25:07 -0400

    Add constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+const ATTACKING  = exports.ATTACKING = 'attacking';
+const HEALING = exports.HEALING = 'healing';
+const COUNTER = exports.COUNTER = 'counter';
+const BUF = exports.BUF = 'buf';
+const DEBUF = exports.DEBUF = 'debuf';
+
+const ALLIES = exports.ALLIES = 'allies';
+const ENEMIES = exports.ENEMIES = 'enemies';
