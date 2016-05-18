744ed7ffe9a5d9f735ac18c6d4ee93b10879079b
Joshua Fehler <jsfehler@gmail.com>
2016-05-18 17:46:09 -0400

    Skill effects are functions instead of numbers

--- skills.js
+++ skills.js
@@ -1,54 +1,85 @@
 'use strict';
 const constants = require('./constants');
 
+// Effect functions for each skill
+
+//TODO: Real calculations for effects
+const useSlice = function (user, target) {
+    target.hp += -5 + (user.strength / 2);
+}
+
+const useSwipe = function (user, target) {
+    target.hp += -3 + (user.strength / 2);
+}
+
+const useSmallHeal = function (user, target) {
+    target.hp += 3 + (user.strength / 2);
+}
+
+const useBigHeal = function (user, target) {
+    target.hp += 8 + (user.strength / 2);
+}
+
+const useCounterAttack = function (user, target) {
+    target.hp -= 4 + (user.strength / 2);
+}
+
+const useArmour = function (user, target) {
+    target.endurance += 1 + (user.strength / 2);
+}
+
+const useDrain = function (user, target) {
+    target.strength -= 1 + (user.strength / 2);
+}
+
 module.exports = [
-  {
+{
     name: 'slice',
     type: constants.ATTACKING,
+    effect: useSlice,
     mp: 5,
-    hp: -5,
     targets: constants.ENEMIES
   },
   {
     name: 'swipe',
     type: constants.ATTACKING,
+    effect: useSwipe,
     mp: 3,
-    hp: -3,
     targets: constants.ENEMIES
   },
   {
     name: 'small heal',
     type: constants.HEALING,
+    effect: useSmallHeal,
     mp: 2,
-    hp: 2,
     targets: constants.ALLIES
   },
   {
     name: 'big heal',
     type: constants.HEALING,
     mp: 5,
-    hp: 8,
+    effect: useBigHeal,
     targets: constants.ALLIES
   },
   {
     name: 'counter attack',
     type: constants.COUNTER,
     mp: 4,
-    hp: -4,
+    effect: useCounterAttack,
     targets: constants.ENEMIES
   },
   {
     name: 'armour',
     type: constants.BUF,
     mp: 2,
-    def: 1,
+    effect: useArmour,
     targets: constants.ALLIES
   },
   {
     name: 'drain',
     type: constants.DEBUF,
     mp: 4,
-    att: 1,
+    effect: useDrain,
     targets: constants.ENEMIES
   }
 ];
